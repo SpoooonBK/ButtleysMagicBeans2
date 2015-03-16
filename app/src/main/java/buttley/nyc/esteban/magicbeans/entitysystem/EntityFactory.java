38fec896356fd58311efdaf07b2aeeb8fd8b5cd2
SpoooonBK <erodriguez927@gmail.com>
2015-03-15 22:22:24 -0400

    Finished entity Factory

--- app/src/main/java/buttley/nyc/esteban/magicbeans/entitysystem/EntityFactory.java
+++ app/src/main/java/buttley/nyc/esteban/magicbeans/entitysystem/EntityFactory.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import buttley.nyc.esteban.magicbeans.main.Assets;
 import buttley.nyc.esteban.magicbeans.model.boards.widgets.WidgetTypeEnum;
 import buttley.nyc.esteban.magicbeans.model.characters.CharacterNamesEnum;
+import buttley.nyc.esteban.magicbeans.model.sounds.SoundMapBuilder;
 
 /**
  * Created by Tara on 3/15/2015.
  */
 public class EntityFactory {
 
-    public static Entity getWidget(WidgetTypeEnum widgetType){
+    public static Entity getEntity(WidgetTypeEnum widgetType){
         Map<ComponentType, Component> componentMap =
                 new EnumMap<ComponentType, Component>(ComponentType.class);
 
@@ -44,11 +45,31 @@ public class EntityFactory {
         return new Entity(componentMap);
     }
 
-    public static Entity getCharacter(CharacterNamesEnum name){
+    public static Entity getEntity(CharacterNamesEnum name){
         Map<ComponentType, Component> componentMap =
                 new EnumMap<ComponentType, Component>(ComponentType.class);
 
+        GameCharacterComponent gameCharacterComponent =
+                new GameCharacterComponent(name, name.getCharacterType());
+        StaticGraphicsComponent staticGraphicsComponent =
+                new StaticGraphicsComponent(Assets.getBitmap(name));
+        AnimationComponent animationComponent=
+                new AnimationComponent(Assets.getSprite(name));
+        SoundComponent soundComponent =
+                new SoundComponent(SoundMapBuilder.buildSoundList(name));
 
+
+        componentMap.put(ComponentType.CHARACTER, gameCharacterComponent);
+        componentMap.put(ComponentType.STATIC_GRAPHICS, staticGraphicsComponent);
+        componentMap.put(ComponentType.ANIMATION, animationComponent);
+        componentMap.put(ComponentType.SOUND, soundComponent);
+
+
+        switch (name){
+            case BUTTLEY:{
+                componentMap.put(ComponentType.HOLD_ENTITY, new HoldEntityComponent());
+            }
+        }
 
         return new Entity(componentMap);
     }
---
+++ app/src/main/java/buttley/nyc/esteban/magicbeans/entitysystem/PositionComponent.java
@@ -0,0 +1,46 @@
+package buttley.nyc.esteban.magicbeans.entitysystem;
+
+/**
+ * Created by Tara on 3/15/2015.
+ */
+public class PositionComponent extends Component {
+    private int mStartingXCoordinate;
+    private int mStartingYCoordinate;
+    private int mCurrentXCoordinate;
+    private int mCurrentYCoordinate;
+
+    public PositionComponent() {
+    }
+
+    public int getmStartingXCoordinate() {
+        return mStartingXCoordinate;
+    }
+
+    public void setmStartingXCoordinate(int mStartingXCoordinate) {
+        this.mStartingXCoordinate = mStartingXCoordinate;
+    }
+
+    public int getmStartingYCoordinate() {
+        return mStartingYCoordinate;
+    }
+
+    public void setmStartingYCoordinate(int mStartingYCoordinate) {
+        this.mStartingYCoordinate = mStartingYCoordinate;
+    }
+
+    public int getmCurrentXCoordinate() {
+        return mCurrentXCoordinate;
+    }
+
+    public void setmCurrentXCoordinate(int mCurrentXCoordinate) {
+        this.mCurrentXCoordinate = mCurrentXCoordinate;
+    }
+
+    public int getmCurrentYCoordinate() {
+        return mCurrentYCoordinate;
+    }
+
+    public void setmCurrentYCoordinate(int mCurrentYCoordinate) {
+        this.mCurrentYCoordinate = mCurrentYCoordinate;
+    }
+}
